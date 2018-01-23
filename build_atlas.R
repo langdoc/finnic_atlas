17abf9ddf182d8de2e8ee5f2bff8a1cb4ec17285
Niko Partanen <nikotapiopartanen@gmail.com>
2018-01-23 11:32:59 +0100

    build script

--- build_atlas.R
+++ build_atlas.R
@@ -8,12 +8,14 @@ library(sf)
 library(glue)
 library(bookdown)
 
-unzip("test/kettunen.zip", exdir = "data", overwrite = TRUE)
+# download.file("http://avaa.tdata.fi/adata/kotus/kettunen.zip", "data/kettunen.zip")
+
+unzip("data/kettunen.zip", exdir = "data", overwrite = TRUE)
 
 source("R/parse_kettunen.R")
 source("R/map_finnic.R")
 source("R/write_atlas_page.R")
 
-1:10 %>% walk(write_atlas_page)
+1:20 %>% walk(write_atlas_page)
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = "docs")
